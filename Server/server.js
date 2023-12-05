dd68662d7a37a88321e5ea2c23bc1b974942bc16
JAVM93 <jhonvasquez9367@gmail.com>
2023-12-05 11:11:23 -0500

    Borrar Carpeta CRUD

--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,7 @@ import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import reportRoutes from "./routes/report.js";
-// import { register } from "./controllers/auth.js";
+//import { register } from "./controllers/auth.js";
 import { createReport } from "./controllers/report.js";
 import { verifyToken } from "./middleware/auth.js";
 
